5250e20b6b44bce684a3587b98c2fc27d032af19
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-05-06 00:35:53 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -6,8 +6,12 @@ def help
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end
+
 def list(songs)
-  songs.each do |song|
-    
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
 end
-end
+
+def play(songs)
+  puts 
